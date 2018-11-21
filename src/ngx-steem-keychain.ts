ff05e61d23283a960dc872b27cb2f4d963464c7d
Ondřej Kupka <ondra.cap@gmail.com>
2018-11-21 13:06:07 +0100

    Add missing Steem Keychain API methods
    Added
    
      * requestSignBuffer
      * requestBroadcast
      * requestSignedCall

--- src/ngx-steem-keychain.ts
+++ src/ngx-steem-keychain.ts
@@ -4,4 +4,6 @@ export { SteemKeychainError } from './errors/steem-keychain-error';
 export { InvalidArgumentsError } from './errors/invalid-arguments-error';
 
 export { Response } from './response';
+export { Operation } from './operation';
+
 export * from './steem-keychain.service';
